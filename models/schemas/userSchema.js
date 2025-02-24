6ebac24e8784a55d47d868b992ed495eb520c7cb
42 <33488710+FortyTwoFortyTwo@users.noreply.github.com>
2025-02-24 15:14:41 +0000

    Integrate user creation in db with validations

--- models/schemas/userSchema.js
+++ models/schemas/userSchema.js
@@ -2,9 +2,11 @@ import { z } from 'zod';
 
 const bookID = z.string();
 
-const User = z.object({
+export const userSchema = z.object({
     username: z.string(),
-    id: z.number(),
-    likes: z.array(bookID),
-    dislikes: z.array(bookID)
+    id: z.number().default(0),
+    likes: z.array(bookID).default([]),
+    dislikes: z.array(bookID).default([])
   });
+
+export const usersSchema = z.array(userSchema);

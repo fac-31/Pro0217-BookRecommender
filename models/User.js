2615682389bcf37aa1d82a471e7a2ad2bc092585
josephpotashnik <joseph.potashnik@gmail.com>
2025-02-24 15:55:28 +0000

    changed following merge from main, User => userSchema

--- models/User.js
+++ models/User.js
@@ -1,12 +1,12 @@
 import { fetchAPI } from '../models/api.js';
-import { User } from './schemas/userSchema.js';
+import { userSchema } from './schemas/userSchema.js';
 
 export async function getUser(username)
 {
     try{
         const requestUserString = "users?username="  + username;
         const user = await fetchAPI(req, requestUserString, "GET");
-        return User.parse(user);
+        return userSchema.parse(user);
     }
     catch (error)
     {

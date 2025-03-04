a44664a807e9197cffaad6460f6f9db1e0d10b21
chrislush <133811141+chrislush@users.noreply.github.com>
2025-03-04 16:41:06 +0000

    Added partial template for dynamic creation of links to friends' reading lists

--- public/friends.js
+++ public/friends.js
@@ -1,3 +1,5 @@
+// Dropdown list visibility
+
 const addFriendOption = document.querySelector(".add-friend");
 const remainingUsersContainer = document.querySelector(".remaining-users");
 
@@ -6,7 +8,26 @@ addFriendOption.onmouseenter = () => {
 };
 
 addFriendOption.onmouseleave = () => {
-  if (!remainingUsersContainer.matches(":hover")) {
-    document.querySelector(".remaining-users").classList.add("hidden");
+  if (remainingUsersContainer.matches(":hover")) {
+    return;
+  } else {
+    remainingUsersContainer.classList.add("hidden");
   }
 };
+
+remainingUsersContainer.onmouseleave = () => {
+  if (addFriendOption.matches(":hover")) {
+    return;
+  } else {
+    remainingUsersContainer.classList.add("hidden");
+  }
+};
+
+// Dynamically create links to friends' reading lists
+
+const friendsContainer = document.querySelector(".friends");
+const newFriendUsername = "Bob";
+
+const newFriendLink = document.createElement("a");
+newFriendLink.innerText = `${newFriendUsername}`;
+friendsContainer.appendChild(newFriendLink);

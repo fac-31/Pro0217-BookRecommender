d5d4954208d36de92a8af5fd07e6356de56479a0
Joseph Potashnik <joseph.potashnik@gmail.com>
2025-03-06 13:39:07 +0000

    using same dynamic DOM elements for recommendations both in library_interior and in my_books

--- public/library-interior/script.js
+++ public/library-interior/script.js
@@ -26,99 +26,6 @@ bookPreferenceForm.onsubmit = (e) => {
 let foundBooks;
 let bookData;
 
-const createButtonElements = (bookDiv, index) => {
-  
-  const acceptButton = document.createElement('button');
-  acceptButton.classList.add("book-btn", "accept", "hidden");
-  acceptButton.innerHTML = "&#10004;"; // Renders ✓
-
-  acceptButton.onclick = () => {
-    judgementPassed(index+1, 'likes');
-
-  }
-
-  const acceptButtonLabel = document.createElement('div');
-  acceptButtonLabel.classList.add("accept-btn-label", "hidden");
-  acceptButtonLabel.innerHTML= "Add book to readlist";
-
-  acceptButton.onmouseenter = () => {
-    acceptButtonLabel.classList.toggle("hidden");
-  }
-  acceptButton.onmouseleave = () => {
-    acceptButtonLabel.classList.toggle("hidden");
-  }
- 
-  const rejectButton = document.createElement('button');
-  rejectButton.classList.add("book-btn", "reject", "hidden");
-  rejectButton.innerHTML = "&#10006;"; // Renders x
-
-  rejectButton.onclick = () => {
-    judgementPassed(index+1, 'dislikes');
-  }
-
-  const rejectButtonLabel = document.createElement('div');
-  rejectButtonLabel.classList.add("reject-btn-label", "hidden");
-  rejectButtonLabel.innerHTML= "Not interested";
-
-  rejectButton.onmouseenter = () => {
-    rejectButtonLabel.classList.toggle("hidden");
-  }
-  rejectButton.onmouseleave = () => {
-    rejectButtonLabel.classList.toggle("hidden");
-  }
- 
-  bookDiv.appendChild(acceptButton);
-  bookDiv.appendChild(acceptButtonLabel);
-  bookDiv.appendChild(rejectButton);
-  bookDiv.appendChild(rejectButtonLabel);
-
-    // Reveal "want to read" and "not interested" buttons
-    bookDiv.onmouseenter = () => {
-        acceptButton.classList.toggle("hidden");
-        rejectButton.classList.toggle("hidden");
-    };
-    // Hide "want to read" and "not interested" buttons
-    bookDiv.onmouseleave = () => {
-      acceptButton.classList.toggle("hidden");
-      rejectButton.classList.toggle("hidden");
-  };
-
-}
-const createBookElements = (data, length, bookRecommendationContainer) => {
-
-  for (let i = 0; i < length; i++) {
-
-    const book = data.books[i];
-    const bookDiv = document.createElement('div');
-    bookDiv.classList.add('book');
-
-    //add image
-    const img = document.createElement('img');
-    img.src = book.cover;
-    img.alt = book.title;
-    bookDiv.appendChild(img);
-
-    bookDiv.onclick = () => {
-      bookInfoContainer.classList.remove("hidden");
-      document.getElementById(
-        "title"
-      ).innerText = `Title: ${book.title}`;
-      document.getElementById(
-        "author"
-      ).innerText = `Author: ${book.author}`;
-      document.getElementById(
-        "year"
-      ).innerText = `Year: ${book.year}`;
-      document.getElementById(
-        "reason"
-      ).innerText = `Reason: ${book.reason_for_recommendation}`;
-    };
-
-    createButtonElements(bookDiv, i);
-    bookRecommendationContainer.appendChild(bookDiv);
-  }
-
-} 
 
 const checkLibrary = async () => {
   try {
@@ -135,7 +42,7 @@ const checkLibrary = async () => {
 
     const length = 4;
     const bookRecommendationContainer = document.getElementById('user-prompt-recommendations-container');
-    createBookElements(data, length, bookRecommendationContainer);
+    createBookElements(data, length, bookRecommendationContainer, bookInfoContainer);
 
     foundBooks = true;
     comeBack();
@@ -153,28 +60,4 @@ const goBack = () => {
   setTimeout(() => {
     pageTransitionFunc("/index.html");
   }, 2000);
-};
-
-const judgementPassed = (bookNum, key) => {
-  const dataToSend = {
-    user_id: localStorage.getItem("userID"),
-    book: bookData.books[bookNum - 1],
-    key: key,
-    add: true,
-  };
-
-  fetch("/users/update-book", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(dataToSend),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
 };

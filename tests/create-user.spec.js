b69302a863611ef16881ce17c894808fd784c1f4
42 <33488710+FortyTwoFortyTwo@users.noreply.github.com>
2025-03-17 16:10:06 +0000

    Add 2 more tests

---
+++ tests/create-user.spec.js
@@ -0,0 +1,25 @@
+import { test, expect } from '@playwright/test';
+
+test('Create user', async ({ page }) => {
+    // Login as Oskar
+    await page.goto('http://localhost:3000/');
+    await page.getByRole('textbox', { name: 'ENTER YOUR FIRST NAME BELOW' }).click();
+    await page.getByRole('textbox', { name: 'ENTER YOUR FIRST NAME BELOW' }).fill('Oskar');
+    await page.getByRole('button', { name: 'SUBMIT' }).click();
+
+    // Logout
+    await page.getByRole('link', { name: 'Log out' }).click();
+
+    // Login as Henson
+    await page.getByRole('textbox', { name: 'ENTER YOUR FIRST NAME BELOW' }).click();
+    await page.getByRole('textbox', { name: 'ENTER YOUR FIRST NAME BELOW' }).fill('Henson');
+    await page.getByRole('button', { name: 'SUBMIT' }).click();
+
+    // Add Oskar as friend
+    await page.getByText('My Friends').click();
+    await page.getByText('Add friend').click();
+    await page.getByRole('link', { name: 'Oskar' }).click();
+    
+    // Open Oskar page
+    await page.getByRole('link', { name: 'Oskar' }).click();
+  });

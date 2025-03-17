ff19275dc04a28f6e86cae3c1873819a41f7ccd6
42 <33488710+FortyTwoFortyTwo@users.noreply.github.com>
2025-03-17 16:15:57 +0000

    I always forget to format

--- tests/recommendations.spec.js
+++ tests/recommendations.spec.js
@@ -1,27 +1,27 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('Use recommendations', async ({ page }) => {
-  test.setTimeout(60000);  // 60 seconds
-  
-  // Login
-  await page.goto('http://localhost:3000/');
-  await page.getByRole('textbox', { name: 'ENTER YOUR FIRST NAME BELOW' }).click();
-  await page.getByRole('textbox', { name: 'ENTER YOUR FIRST NAME BELOW' }).fill('Oskar');
-  await page.getByRole('button', { name: 'SUBMIT' }).click();
+test("Use recommendations", async ({ page }) => {
+	test.setTimeout(60000); // 60 seconds
 
-  // Enter a preference
-  await page.getByRole('textbox', { name: 'Enter your book preferences' }).click();
-  await page.getByRole('textbox', { name: 'Enter your book preferences' }).fill('London');
-  await page.getByRole('button', { name: 'SUBMIT' }).click();
-  await page.getByRole('img').first().click();
+	// Login
+	await page.goto("http://localhost:3000/");
+	await page.getByRole("textbox", { name: "ENTER YOUR FIRST NAME BELOW" }).click();
+	await page.getByRole("textbox", { name: "ENTER YOUR FIRST NAME BELOW" }).fill("Oskar");
+	await page.getByRole("button", { name: "SUBMIT" }).click();
 
-  // Likes a book and wait for response
-  const responsePromise = page.waitForRequest("http://localhost:3000/users/update-book");
-  await page.getByRole('button', { name: '✓' }).click();
-  const response = await responsePromise;
+	// Enter a preference
+	await page.getByRole("textbox", { name: "Enter your book preferences" }).click();
+	await page.getByRole("textbox", { name: "Enter your book preferences" }).fill("London");
+	await page.getByRole("button", { name: "SUBMIT" }).click();
+	await page.getByRole("img").first().click();
 
-  // Go to myBooks and dislike it 
-  await page.getByRole('link', { name: 'My Books' }).click();
-  await page.getByRole('img').first().click();
-  await page.getByRole('button', { name: '✕' }).click();
+	// Likes a book and wait for response
+	const responsePromise = page.waitForRequest("http://localhost:3000/users/update-book");
+	await page.getByRole("button", { name: "✓" }).click();
+	const response = await responsePromise;
+
+	// Go to myBooks and dislike it
+	await page.getByRole("link", { name: "My Books" }).click();
+	await page.getByRole("img").first().click();
+	await page.getByRole("button", { name: "✕" }).click();
 });

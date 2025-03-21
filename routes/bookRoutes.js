3ec6e1597151592893e8a2edb917f30cdd994540
Joseph Potashnik <joseph.potashnik@gmail.com>
2025-03-21 11:13:26 +0000

    update books routes, fixed return value of recommendations

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -3,16 +3,9 @@ const router = express.Router();
 import {
 	fetchBooksByIDs,
 	fetchBooksByKey,
-	createBook,
-	updateBook,
-	deleteBook,
 } from "../controllers/bookController.js";
 
 router.post("/fetchBooksByIDs", fetchBooksByIDs);
 router.post("/fetchBooksByKey", fetchBooksByKey);
-
-router.post("/add", createBook);
-router.post("/update/:id", updateBook);
-router.post("/delete/:id", deleteBook);
 
 export { router as bookRoutes };
